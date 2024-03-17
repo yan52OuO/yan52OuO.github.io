4db0d8849fd05d92155e5c53d23c9cb1fdbc34c1
yan <2742435247@qq.com>
2024-03-17 16:18:29 +0800

    Site updated: 2024-03-17 16:18:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/11/hello-world/"];function toRandomPost(){
+var posts=["2024/03/11/hello-world/","2024/03/17/第一次博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
